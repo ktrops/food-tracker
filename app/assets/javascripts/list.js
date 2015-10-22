2a5409e50dfd62aa9cca503977e08f29ec94a3e2
Katie Atrops <katrops@gmail.com>
2015-10-21 19:16:37 -0700

    dynamically adding text boxes

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-  $("form").submit(function( event ) {
-    $(".listForm").after(".listForm")
+  var rowNum = 0
+  $("#addAnother").click(function( event ) {
+    rowNum++
+    var $selectLastInput = $(".inputItem").last();
+    var $selectLastLabel = $(".labelItem").last();
+    var nextHtmlLabel = $selectLastLabel.clone();
+    nextHtmlLabel.attr("for", "product_name" + rowNum);
+    console.log(nextHtmlLabel);
+    $($selectLastInput).after(nextHtmlLabel);
+
+    var $selectLatestLabel = $(".labelItem").last()
+    var nextHtmlInput = $selectLastInput.clone();
+    nextHtmlInput.attr("id", "product_name" + rowNum);
+    $($selectLatestLabel).after(nextHtmlInput);
 
   })
+
 })
