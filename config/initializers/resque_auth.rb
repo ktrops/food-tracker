106fafc80ada9d5522a2e810431315f79e2d06ec
Katie Atrops <katrops@gmail.com>
2015-10-29 15:39:57 -0700

    add resque.redis setting to initializer

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,3 +1,9 @@
+if Rails.env.production?
+  Resque.redis = 'food-tracker-001.mg1fnh.0001.usw2.cache.amazonaws.com:6379'
+else
+  Resque.redis = 'localhost:6379'
+end
+
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
   password == "katie"
 end
