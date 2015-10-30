68d14db4b8296a39da86a7475d396f53bc11ec6e
Katie Atrops <katrops@gmail.com>
2015-10-30 13:34:35 -0700

    changed so emails will be send to me in production

--- app/workers/check_expiration_date.rb
+++ app/workers/check_expiration_date.rb
@@ -4,9 +4,11 @@ class CheckExpirationDate
   def self.perform
     UserProduct.all.each do |user_product|
       unless user_product.dop_expiration_date_min.nil?
-        if user_product.dop_expiration_date_min <= Date.tomorrow && user_product.user.id == 1
+        if user_product.dop_expiration_date_min <= Date.tomorrow
+          if user_product.user.id == 1 || user_product.user.id == 4
 
-          ModelMailer.expiration_notification(user_product.product.name, user_product.user).deliver
+            ModelMailer.expiration_notification(user_product.product.name, user_product.user).deliver
+          end
         end
       end
     end
